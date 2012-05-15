6751e9812c6bc2d10be225828b085477b0acd4fe
ISeeDEDPpl <bgates256@gmail.com>
2012-05-14 21:35:26 -0500

    no, this was not the right lines to commit =/ the 'health' is represented as "1" (100%) so rounding this value to no decimal places is incorrect.

--- Questor.Modules/Combat/Drones.cs
+++ Questor.Modules/Combat/Drones.cs
@@ -54,7 +54,7 @@ namespace Questor.Modules.Combat
             if (!Cache.Instance.ActiveDrones.Any())
                 return 0;
 
-            return Math.Round(Cache.Instance.ActiveDrones.Sum(d => d.ShieldPct),0);
+            return Cache.Instance.ActiveDrones.Sum(d => d.ShieldPct);
         }
 
         private double GetArmorPctTotal()
@@ -62,7 +62,7 @@ namespace Questor.Modules.Combat
             if (!Cache.Instance.ActiveDrones.Any())
                 return 0;
 
-            return Math.Round(Cache.Instance.ActiveDrones.Sum(d => d.ArmorPct),0);
+            return Cache.Instance.ActiveDrones.Sum(d => d.ArmorPct);
         }
 
         private double GetStructurePctTotal()
@@ -70,7 +70,7 @@ namespace Questor.Modules.Combat
             if (!Cache.Instance.ActiveDrones.Any())
                 return 0;
 
-            return Math.Round(Cache.Instance.ActiveDrones.Sum(d => d.StructurePct),0);
+            return Cache.Instance.ActiveDrones.Sum(d => d.StructurePct);
         }
 
         /// <summary>
